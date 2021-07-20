23c7ffdb2946a58afe71286b998184268a69f56d
Dmytro Piskun <dmytro.piskun14@gmail.com>
2021-07-20 14:53:48 +0300

    add yup validation

--- src/app/controllets/userControllers.ts
+++ src/app/controllets/userControllers.ts
@@ -4,14 +4,16 @@ import { usersRepository } from "../repositories";
 import { compareSync, genSaltSync, hashSync } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { env } from "../../utils/env/env";
+import { userSchema } from "../validators/yup";
 export const registationController = async (req: Request, res: Response) => {
-  if (
-    typeof req.body.password === "string" &&
-    typeof req.body.email === "string"
-  ) {
-    const userEmail = req.body.email;
-    const userPassword = req.body.password;
-    if (typeof userPassword != "undefined") {
+  userSchema
+    .isValid({
+      email: req.body.email,
+      password: req.body.password,
+    })
+    .then(async () => {
+      const userEmail = req.body.email;
+      const userPassword = req.body.password;
       const salt = genSaltSync(10);
       const hash = hashSync(userPassword, salt);
       const user = new userModel({
@@ -21,33 +23,33 @@ export const registationController = async (req: Request, res: Response) => {
       const newUser = await usersRepository.create(user);
       await newUser.save();
       res.status(200).redirect("/");
-    } else {
-      res.status(400).send("bad password");
-    }
-  } else {
-    res.status(400).send("bad request");
-  }
+    })
+    .catch(() => {
+      res.status(400).send("bad request");
+    });
 };
 
 export const loginController = async (req: Request, res: Response) => {
-  if (
-    typeof req.body.password === "string" &&
-    typeof req.body.email === "string"
-  ) {
-    const userEmail = req.body.email;
-    const userPassword = req.body.password;
-    const authingUser = await userModel.findOne({ email: userEmail });
-
-    if (compareSync(userPassword, authingUser.password)) {
-      const token = sign({ email: userEmail }, env.tokenSecret, {
-        expiresIn: 60 * 60,
-        algorithm: "HS384",
-      });
-      res.status(200).json({ accessToken: token });
-    } else {
-      res.status(401).send("Unauthorized");
-    }
-  } else {
-    res.status(400).send("bad request");
-  }
+  userSchema
+    .isValid({
+      email: req.body.email,
+      password: req.body.password,
+    })
+    .then(async () => {
+      const userEmail = req.body.email;
+      const userPassword = req.body.password;
+      const authingUser = await userModel.findOne({ email: userEmail });
+      if (compareSync(userPassword, authingUser.password)) {
+        const token = sign({ email: userEmail }, `${env.tokenSecret}`, {
+          expiresIn: 60 * 60,
+          algorithm: "HS384",
+        });
+        res.status(200).json({ accessToken: token });
+      } else {
+        res.status(401).send("Unauthorized");
+      }
+    })
+    .catch(() => {
+      res.status(400).send("bad request");
+    });
 };
