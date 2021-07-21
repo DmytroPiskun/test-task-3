d70f593a1a04e087f3e734bf6000c221ea3d4f29
Dmytro Piskun <dmytro.piskun14@gmail.com>
2021-07-21 11:16:37 +0300

    add delete user function

--- src/app/routers/router.ts
+++ src/app/routers/router.ts
@@ -7,6 +7,7 @@ import {
   testValidController,
 } from "../controllets/baseControllers";
 import {
+  deleteAccountContoller,
   loginController,
   registationController,
 } from "../controllets/userControllers";
@@ -17,6 +18,10 @@ export const router: Router = Router();
 router.get("/", defaultController);
 router.post("/registration", isValidData, registationController);
 router.post("/login", isValidData, loginController);
+
+router.post("/registration", registationController);
+router.post("/login", loginController);
+router.post("/delete-account", isAuth, deleteAccountContoller);
 
 router.get("/testpass", isAuth, testController);
 router.get("/testvalid", isValidData, testValidController);
