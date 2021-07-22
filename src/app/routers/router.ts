65f435924823a1363d62915741abf82bc4921eaf
Dmytro Piskun <dmytro.piskun14@gmail.com>
2021-07-22 15:10:54 +0300

    fixes after review 2

--- src/app/routers/router.ts
+++ src/app/routers/router.ts
@@ -10,14 +10,13 @@ import {
   loginController,
   registationController,
 } from "../controllets/userControllers";
-import app from "../../server";
 // Export module for registering router in express app
 export const router: Router = Router();
 
 // Define your routes here
 router.get("/", defaultController);
-router.post("/registration", isValidData(), registationController);
-router.post("/login", isValidData(), loginController);
+router.post("/registration", isValidData, registationController);
+router.post("/login", isValidData, loginController);
 
 router.get("/testpass", isAuth, testController);
-router.get("/testvalid", isValidData(), testValidController);
+router.get("/testvalid", isValidData, testValidController);
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,11 @@ import configuration from "../config.json";
 import { connect } from "mongoose";
 import passport from "passport";
 import { env } from "./utils/env/env";
-import { Router } from "express";
-
+import { myPassort } from "./app/middleware/passport";
 connect(env.dbLink)
-  .then(() => {
+  .then(async () => {
     app.use(passport.initialize());
-    require("./app/middleware/passport")(passport);
-
-    // Start the application by listening to specific port
+    myPassort(passport);
     const port = Number(env.port || configuration.PORT || 8080);
     app.listen(port, () => {
       console.info("Express application started on port: " + port);
