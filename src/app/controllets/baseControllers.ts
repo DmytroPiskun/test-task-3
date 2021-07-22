42f59900615368a435797d209b33d1752a7c9552
Dmytro Piskun <dmytro.piskun14@gmail.com>
2021-07-22 11:12:37 +0300

    done fixes after review

--- src/app/controllets/baseControllers.ts
+++ src/app/controllets/baseControllers.ts
@@ -9,5 +9,13 @@ export const defaultController = async (_req: Request, res: Response) => {
 };
 
 export const testController = async (req: Request, res: Response) => {
-  res.status(200).send({ test: "test" });
+  res.status(200).json({ test: "test" });
+};
+
+export const testValidController = async (req: Request, res: Response) => {
+  try {
+    res.status(200).json({ testValid: "valid" });
+  } catch (e) {
+    res.status(400).send(e);
+  }
 };
