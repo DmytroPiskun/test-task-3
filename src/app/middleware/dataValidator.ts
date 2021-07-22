0a64ec4f6a57a02e6372d6cd4519986efb867d95
Dmytro Piskun <dmytro.piskun14@gmail.com>
2021-07-22 16:59:46 +0300

    fixes after review 4

--- src/app/middleware/dataValidator.ts
+++ src/app/middleware/dataValidator.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 import { IUser } from "../interfaces";
-import { ObjectSchema } from "yup";
+import { AnySchema, ObjectSchema } from "yup";
 export const dataValidator =
-  (schema: ObjectSchema<{}>) =>
-  async (req: Request<{}, IUser>, res: Response, next: Function) => {
+  <TSchema extends AnySchema>(schema: TSchema) =>
+  async (req: Request, res: Response, next: Function) => {
     try {
       await schema.validate(req.body);
       return next();
