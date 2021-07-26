fb9c2811d41d8ed20c0e4769a1fc461517bbc699
Dmytro Piskun <dmytro.piskun14@gmail.com>
2021-07-26 12:22:07 +0300

    fixes after review 3

--- src/app/validators/pageSchema.validator.ts
+++ src/app/validators/pageSchema.validator.ts
@@ -1,8 +1,6 @@
 import * as yup from "yup";
 
 export const pageSchema = yup.object().shape({
-  // page: yup.string().matches(/^\d+$/).required(),
-  // perPage: yup.string().matches(/^\d+$/),
   page: yup.number().required(),
   perPage: yup.number().required(),
 });
